c3f87c23550c404a258e728a159c43b85aca1b01
정평두 <kotran@naver.com>
2017-12-16 07:20:28 +0900

    Initial Commit

---
+++ customtable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  customtable
+//
+//  Created by 정평두 on 2017. 12. 16..
+//  Copyright © 2017년 moontomoon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
